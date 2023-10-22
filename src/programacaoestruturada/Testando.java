4c64dfd45d64a73bc278c592657a400acaccca42
Lucijane <lucijanejusto@gmail.com>
2023-10-22 16:47:35 -0300

    CLASSE DE TESTE ADICIONADA

---
+++ src/programacaoestruturada/Testando.java
@@ -0,0 +1,17 @@
+package programacaoestruturada;
+
+public class Testando {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=40 ,b=40;
+		int soma = a+b;
+		
+		System.out.println(soma);
+		
+		
+		
+		
+	}
+
+}
